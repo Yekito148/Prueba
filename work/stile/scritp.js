7137b5d32536af0a07703ec8f05c85536529fb69
Yesid Sanchez <nayrock62@gmail.com>
2025-08-22 19:42:51 -0500

    Final encabezado

--- work/stile/scritp.js
+++ work/stile/scritp.js
@@ -1,36 +1,61 @@
 const header = document.getElementById("header");
 const foto = document.getElementById("foto_perfil");
 const nombre = document.getElementById("nombre");
+const titulo = document.getElementById("titulo");
+const navbar = document.querySelector(".navbar");
 
 window.addEventListener("scroll", () => {
     const scrollY = window.scrollY;
     const maxScroll = 600;
     const progress = Math.min(scrollY / maxScroll, 1);
 
-    const headerHeight = 50 - (progress * 40);
+    const headerHeight = 30 - (progress * 20);
     header.style.height = `${headerHeight}vh`;
 
     const fotoSize = 180 - (120 * progress);
     foto.style.width = `${fotoSize}px`;
     foto.style.height = `${fotoSize}px`;
 
-    const fontSize = 2 -(0.8 *progress);
+    const fontSize = 2 - (0.8 * progress);
     nombre.style.fontSize = `${fontSize}rem`;
+    titulo.style.fontSize = `${fontSize}rem`;
 
-    if (progress >= 0.6){
+    if (progress >= 0.6) {
         header.style.flexDirection = "row";
-        header.style.justifyContent = "flex-start";
+        header.style.justifyContent = "space-between";
         header.style.alignItems = "center";
+
         nombre.style.marginTop = "0";
         nombre.style.marginLeft = "15px";
-    }
-    else{
+
+        titulo.style.marginTop = "0";
+        titulo.style.marginLeft = "15px";
+
+        navbar.style.position = "flex-end"; 
+        navbar.style.marginLeft = "auto"; 
+    } else {
+       
         header.style.flexDirection = "column";
         header.style.justifyContent = "center";
         header.style.alignItems = "center";
+
         nombre.style.marginTop = "15px";
         nombre.style.marginLeft = "0";
-    }
-    
+            
+        titulo.style.marginTop = "15px";
+        titulo.style.marginLeft = "0";
 
-})
+        navbar.style.position = "relative";
+        navbar.style.top = "auto";
+        navbar.style.marginLeft = "0";
+    }
+});
+document.querySelectorAll('.navbar-nav .nav-link').forEach(link => {
+    link.addEventListener('click', () =>{
+        const navbarCollapse = document.querySelector('.navbar-collapse');
+        const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+        if(bsCollapse){
+            bsCollapse.hide();
+        }
+    });
+});
